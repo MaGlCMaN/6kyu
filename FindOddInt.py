c4466747095186d00cede1b5ac0437bcf0920060
MaGiCMaN <maglc_man@hotmail.com>
2018-06-28 13:09:19 -0500

    Add FindOddInt.py

---
+++ FindOddInt.py
@@ -0,0 +1,22 @@
+def find_it(seq):
+    my_dict = {}
+    num = 0
+
+    for i in seq:
+        my_dict[i] = 0
+    
+    for i in seq:
+        try:
+            while seq[num] == i:
+                my_dict[i] += 1
+                if num <= len(seq)-1:
+                    num += 1
+                else:
+                    break
+        except IndexError:
+            pass
+        
+    for i in my_dict:
+        #x = my_dict[i] % 2
+        if my_dict[i] % 2 != 0:
+            return i
